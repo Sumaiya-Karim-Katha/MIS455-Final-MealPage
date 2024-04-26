4c8065dbe4e8f619743caaf20e802543373dfca2
Sumaiya Karim Katha <2010189@iub.edu.bd>
2024-04-26 21:51:05 +0600

    remove modal function

--- script.js
+++ script.js
@@ -96,10 +96,5 @@ function displayAll(itemList) {
             </div>`;
         container.appendChild(divbox);
     }
-    const myModal = document.getElementById('myModal')
-    const myInput = document.getElementById('myInput')
-
-    myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus()
-    })
+    
 }
